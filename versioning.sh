84591bed22c27af0fc86945f284a2be9707d85f4
Endy B <endyb.dev@gmail.com>
2023-10-05 19:02:11 +1100

    fix versioning.sh

--- versioning.sh
+++ versioning.sh
@@ -2,4 +2,6 @@
 
 echo "Hello increment Version.. v0.1.0 :)"
 echo "This is an initial test"
-export DEFAULT_VERSION='v0.1.0'
+DEFAULT_VERSION='v0.1.0'
+
+echo "default_version=$(echo '$DEFAULT_VERSION')" >> $GITHUB_OUTPUT
